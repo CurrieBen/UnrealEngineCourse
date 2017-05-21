8e73256eff4009f4ce4f25224e7075ed89c02787
Ben Currie <Ben_currie_173@hotmail.co.uk>
2017-05-21 12:55:43 +0100

    fixed bug - game logic working

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -44,12 +44,12 @@ FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
 	FBullCowCount BullCowCount;
 	//loop through all letters in the guess
 	int32 HiddenWordLength = MyHiddenWord.length();
-	for (int32 i = 0; i < HiddenWordLength; i++) {
+	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++) {
 		//compare the letters against the hidden word
-		for (int32 j = 0; j < HiddenWordLength; j++) {
+		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++) {
 			//if they match then
-			if (Guess[i] == MyHiddenWord[i]) {
-				if (i == j) { //if they are in the same place add bulls
+			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
+				if (MHWChar == GChar) { //if they are in the same place add bulls
 					BullCowCount.Bulls++;
 				}
 				else {
